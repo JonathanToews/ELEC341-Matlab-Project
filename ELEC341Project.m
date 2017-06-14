06932c13e13086db409b8c572f0dd8450f7d385f
isaacj92 <isaac.jacobsen.1992@gmail.com>
2017-06-13 19:41:44 -0700

    For matlab - Added some more comments to parts - Fixed up some of the matrices that were sym instead of double matrices - Got the plot working for Question 6
    For write-up
    - Created the write-up
    - Wrote up some quick notes on why the open-loop system was so poor

--- ELEC341Project.m
+++ ELEC341Project.m
@@ -10,37 +10,53 @@ C = [0 1 0 0]
 D = [0]
 F = [0; 0; 0; 1/J2]         % disturbance torque vector
 
+
 % Question #2: Compute Open-Loop Eigenvalues
 eigs = eig(A)
 
+
 % Question #3: Simulate the unit step changes input I and in
 % disturbance torque Td
-sys1 = ss(A, B, C, D);      % define sys1 as the ss with input I
-sys2 = ss(A, F, C, D);      % define sys2 as the ss with input Td
+sys1 = ss(A, B, C, D);      % define sys1 as the ss with input I (Td = 0)
+sys2 = ss(A, F, C, D);      % define sys2 as the ss with input Td (I = 0)
 
-
-step(sys1, sys2, 2500)            % apply unit step to both ss models, plot
+% Apply unit step to both models and plot
+step(sys1, sys2, 25000)
 title('Unit step changes in the input I and disturbance torque Td');
+
+% Controllability of the open-loop system
+cont = [B A*B A^2*B A^3*B];
+det_cont = det(cont)
 
 
 % Question #4:
 syms K1 K2 K3 K4 s;                 % declare controller variables
 K = [K1, K2, K3, K4];               % gain vector K
 
-PEQ1 = (s+2)*(s+1)*(s^2 +2*s + 2);  % desired closed loop poles polynomial
-PEQ2 = det(s.*eye(4) - (A - B*K));    % closed loop polynomial with feedback
+% We want closed-loop poles of -1, -2, -1+-j. This polynomial provides
+% those poles
+PEQ1 = (s + 2)*(s + 1)*(s^2 + 2*s + 2);
 
-TEMP = coeffs(PEQ1-PEQ2, s);        % coefficients to solve feedback vals
+% This is the polynomial provided by the closed loop polynomial with feedback
+% det(sI - (A - BK))
+PEQ2 = det(s.*eye(4) - (A - B*K));
+
+% In order to find the values of K that will provide the closed-loop poles
+% we want given our state-space model, we must equate the two above
+% equations and solve for K
+TEMP = coeffs(PEQ1 - PEQ2, s);
 [K1, K2, K3, K4] = solve(TEMP(1)==0, TEMP(2)==0, TEMP(3)==0, TEMP(4)==0);
-K = [K1, K2, K3, K4]                % gain vector K
+K = double([K1, K2, K3, K4])        % ensure that K matrix is of type double
 
 
 % Question #5:
-Kr = -1 / (C*inv(A-B*K)*B)
+Kr = double(-1 / (C*inv(A-B*K)*B))  % ensure that Kr matrix is of type double
+
 
 % Question #6:
 sys3 = ss(A-B*K, Kr.*B, C, D);
-step(sys3);
+step(sys3, 20);
+ylim([0 1.5])
 
 % Question #7:
 syms L1 L2 L3 L4;
